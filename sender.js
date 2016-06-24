18aaba3037cfbd64b7050b7a3d022b0ede40008b
Francis Yan <yuyan0@stanford.edu>
2016-06-23 18:57:50 -0700

    Sender sends offer instead

--- sender.js
+++ sender.js
@@ -1,19 +1,71 @@
-var socket = io.connect('http://localhost:3000');
+var localVideo = document.getElementById('localVideo');  
+var localStream;
 
-var myAddr = '10.0.0.2';
-var peerAddr = '10.0.0.1';
+var socket = io.connect('http://localhost:3000');
+var localAddr = '10.0.0.1';
+var remoteAddr; 
+
+var localPC;
+var localOffer;
+var remoteAnswer;
+var offerOptions = {
+  offerToReceiveVideo: 0
+};
 
 function message(text) {
-  this.from = myAddr;
-  this.to = peerAddr;
+  this.from = localAddr;
+  this.to = remoteAddr;
   this.text = text;
 }
 
+function sendOffer() {
+  var servers = null;
+  localPC = new RTCPeerConnection(servers);
+
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({video:true}, onSuccess, onFail);
+  } else {
+    alert('Does not support to get user media');
+  }
+}
+
+function onSuccess(stream) {
+  localVideo.srcObject = stream;
+  localStream = stream;
+  localPC.addStream(localStream);
+
+  localPC.createOffer(offerOptions)
+    .then(function(offer) {
+      console.log('Created offer', offer);
+      localOffer = offer;
+      setOffer();
+      socket.emit('msg', new message(localOffer));
+    });
+}
+
+function onFail() {
+  alert('Failed to get user media!');
+}
+
+function setOffer() {
+  localPC.setLocalDescription(localOffer);
+}
+
+function setAnswer() {
+  localPC.setRemoteDescription(remoteAnswer);
+}
+
 socket.on('connect', function() {
-  socket.emit('addr', myAddr);
-  socket.emit('msg', new message('Request from sender'));
+  socket.emit('addr', localAddr);
   socket.on('msg', function(data) {
-    console.log('Received offer from receiver', data.text);
-    socket.emit('msg', new message('Answer from sender')); 
+    if (!remoteAddr) {
+      remoteAddr = data.from;
+      sendOffer();
+    } else {
+      console.log('Received answer from receiver', data.text);
+      remoteAnswer = data.text;
+      setAnswer();
+    }
   });
 });
+
