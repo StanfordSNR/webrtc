c894cc979d8fce036c326f1e855fb06fa3f6ca01
Francis <francisyyan@stanford.edu>
2016-06-29 00:36:46 -0700

    Structure the app and use Jade

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(3000);
-app.use(express.static(__dirname + '/'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/receiver', function(req, res) {
-  res.sendFile(__dirname + '/receiver.html');
+  res.render('receiver');
 });
 
 app.get('/sender', function(req, res) {
-  // console.log(req.query.addr);
-  res.sendFile(__dirname + '/sender.html');
+  res.render('sender');
 });
 
 var clients = {};
